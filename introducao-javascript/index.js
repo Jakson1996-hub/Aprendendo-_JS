4ebda19eda7862b6e432f97d055fad468a5164de
Jakson1996-hub <josejaksongouveia@gmail.com>
2022-08-19 00:09:56 -0300

    tentando ajustar tabela

--- introducao-javascript/index.js
+++ introducao-javascript/index.js
@@ -1,55 +1,49 @@
 // IMC massa / massa*altura
 
-var pacientes = document.querySelectorAll(".paciente");
+var paciente1 = document.querySelector("#primeiroPaciente"); // Essa linha serve para pegarmos o array do primeiro paciente [nome, peso, altura, gordura e imc]
+var nome = paciente1.querySelector(".info-nome"); 
 
-for(var i = 0;  i< pacientes.length; i ++){
-    var paciente = pacientes[i]
+var peso = paciente1.querySelector('.info-peso');
+var pesoNumber = Number(peso);
+
+    let pesoValido = false;
+    let alturaValida = false;
 
 
-    const pesoPaciente = paciente.querySelector(".info-peso");
-    const peso = pesoPaciente.textContent;
-    
-    let pesoValido = true;
-    
-    const tdAltura = paciente.querySelector(".info-altura");
-    const altura = tdAltura.textContent;
-    
-    let alturaValida = true;
-    
-    // const tdGordura = paciente.querySelector(".info-gordura");
-    // const gordura = tdGordura.textContent;
-    
-    let tdImc = paciente.querySelector(".info-imc")
-    
-    if(peso < 0  ||  peso >= 1000){
-        console.log('Peso inválido')
-         pesoValido = false;
-    }   tdImc.textContent = "Peso inválido";
-    
-    if( altura <0 || altura > 5){
-        console.log("Altura inválida");
-        alturaValida = false;
-        tdImc.textContent = "Altura inválida";
-    }
-    
-    
-    
-    if(alturaValida == true && pesoValido == true){
-    const imc = (peso/(altura*altura)).toFixed(0)
-    let tdImc = paciente.querySelector(".info-imc");
-    tdImc.textContent = imc
-    alert("Seu imc é de :"+ imc)
-    }else{alert("altura ou peso com valores surreais")}
+var altura = paciente1.querySelector('.info-altura');
+var alturaNumber = altura.textContent;
+console.log(alturaNumber)
+
+var imcPreenche = paciente1.querySelector('.info-imc')
+
+
+
+
+
+if (alturaNumber >= 4 && alturaNumber <=0 ){
+
+    imcPreenche.textContent = "Altura inválida"
+    console.log("altura inválida")
 }
 
 
+// if(pesoValido == true && alturaValida == true)
+//     {
+//     var imc = (pesoNumber/(alturaNumber*alturaNumber)).toFixed(0)
+// }else{""}
 
 
-//  console.log(paciente); /// <tr
-//  console.log(peso); /// td =  peso
-//  console.log(altura); // td =  altura
-//  console.log(gordura); // td =  gordura
-//
 
 
-// > </tr>
+// console.log(imcPreenche)
+
+
+
+
+
+
+
+
+// console.log(imc)
+// console.log(pesoNumber+alturaNumber)
+// console.log(`o Nome do Paciente é ${nome},  pesa ${peso}Kg e mede ${altura}`)
